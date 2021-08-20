8b624e57f97eaf8edc0f11421702583f1ac563b9
Feli <felinegri5@gmail.com>
2021-08-20 16:38:13 -0300

    AmmoCheck Upload

---
+++ AmmoCheck/AmmoCheck.cs
@@ -0,0 +1,82 @@
+﻿using Cysharp.Threading.Tasks;
+using Microsoft.Extensions.Localization;
+using OpenMod.API.Plugins;
+using OpenMod.Unturned.Plugins;
+using SDG.Unturned;
+using System;
+
+[assembly: PluginMetadata("Feli.AmmoCheck", DisplayName = "AmmoCheck")]
+namespace AmmoCheck
+{
+    public class AmmoCheck : OpenModUnturnedPlugin
+    {
+        private readonly IStringLocalizer stringLocalizer;
+
+        public AmmoCheck(
+            IStringLocalizer stringLocalizer,
+            IServiceProvider serviceProvider) : base(serviceProvider) 
+        {
+            this.stringLocalizer = stringLocalizer;
+        }
+
+        protected override UniTask OnLoadAsync()
+        {
+            PlayerEquipment.OnInspectingUseable_Global += PlayerEquipment_OnInspectingUseable_Global;
+            return UniTask.CompletedTask;
+        }
+
+        private void PlayerEquipment_OnInspectingUseable_Global(PlayerEquipment obj)
+        {
+            if(obj.useable is UseableGun)
+            {
+                string message = String.Empty;
+
+                var magazineId = obj.state[8];
+
+                if (magazineId != 0)
+                {
+                    var magazine = (ItemMagazineAsset)Assets.find(EAssetType.ITEM, magazineId);
+                    var ammo = obj.state[10];
+
+                    var ammoQuantity = (double)ammo / (double)magazine.amount;
+
+                    if (ammoQuantity >= 0.9)
+                    {
+                        message = stringLocalizer["ammo:full"];
+                    }
+                    else if(ammoQuantity < 0.9 && ammoQuantity >= 0.8)
+                    {
+                        message = stringLocalizer["ammo:moreHalf"];
+                    }
+                    else if (ammoQuantity < 0.8 && ammoQuantity >= 0.5)
+                    {
+                        message = stringLocalizer["ammo:aboutHalf"];
+                    }
+                    else if(ammoQuantity < 0.5 && ammoQuantity >= 0.35)
+                    {
+                        message = stringLocalizer["ammo:lessHalf"];
+                    }
+                    else if(ammoQuantity < 0.35 && ammoQuantity >= 0.1)
+                    {
+                        message = stringLocalizer["ammo:almostEmpty"];
+                    }
+                    else
+                    {
+                        message = stringLocalizer["ammo:empty"];
+                    }
+                }
+                else
+                    message = stringLocalizer["ammo:empty"];
+
+                EffectManager.sendUIEffect(2355, 333, obj.player.channel.owner.transportConnection, true, message);
+            }
+        }
+
+        protected override UniTask OnUnloadAsync()
+        {
+            PlayerEquipment.OnInspectingUseable_Global -= PlayerEquipment_OnInspectingUseable_Global;
+            return UniTask.CompletedTask;
+        }
+    }
+   
+}
---
+++ AmmoCheck/Events/UnturnedPlayerConnectedEventListener.cs
@@ -0,0 +1,27 @@
+﻿using Cysharp.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using OpenMod.API.Eventing;
+using OpenMod.Unturned.Players.Connections.Events;
+using SDG.Unturned;
+using System.Threading.Tasks;
+
+namespace AmmoCheck.Events
+{
+    public class UnturnedPlayerConnectedEventListener : IEventListener<UnturnedPlayerConnectedEvent>
+    {
+        private readonly IConfiguration configuration;
+        public UnturnedPlayerConnectedEventListener(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task HandleEventAsync(object sender, UnturnedPlayerConnectedEvent @event)
+        {
+            if(configuration.GetSection("hideDefaultGunStatus").Get<bool>())
+            {
+                await UniTask.SwitchToMainThread();
+                @event.Player.Player.disablePluginWidgetFlag(EPluginWidgetFlags.ShowUseableGunStatus);
+            }
+        }
+    }
+}
